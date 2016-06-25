ce1c4e3c6fa44e63e57e1f573a78173816a22f2a
BlackZack <fernando.roque89@gmail.com>
2016-06-25 19:30:03 +0200

    empezado paciente por dnis

---
+++ src/main/java/com/frracm/asclepio/model/Paciente.java
@@ -0,0 +1,9 @@
+package com.frracm.asclepio.model;
+
+public class Paciente {
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private Long numeroHistoriaClinica;
+	
+}
